2bb56c4278ca8399b8e77eca6f3c9310c34e92db
RossWoods41 <54415033+RossWoods41@users.noreply.github.com>
2019-10-29 03:42:11 -0400

    Added Health Bar
    I added a health bar

---
+++ .cache/behavior-hx/SceneEvents_1.hx
@@ -0,0 +1,130 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+import com.stencyl.graphics.ScaleMode;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.Script.*;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+import com.stencyl.models.Joystick;
+
+import com.stencyl.Config;
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
+import com.stencyl.utils.motion.*;
+import com.stencyl.utils.Utils;
+
+import openfl.ui.Mouse;
+import openfl.display.Graphics;
+import openfl.display.BlendMode;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.events.TouchEvent;
+import openfl.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+import box2D.collision.shapes.B2Shape;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
+
+class SceneEvents_1 extends SceneScript
+{
+	
+	
+	public function new(dummy:Int, dummy2:Engine)
+	{
+		super();
+		
+	}
+	
+	override public function init()
+	{
+		
+		/* ======================== When Creating ========================= */
+		Engine.engine.setGameAttribute("Health", 3);
+		Engine.engine.setGameAttribute("Score", 0);
+		
+		/* ========================= When Drawing ========================= */
+		addWhenDrawingListener(null, function(g:G, x:Float, y:Float, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				g.setFont(getFont(25));
+				g.strokeColor = Utils.getColorRGB(0,0,0);
+				g.drawString("" + "Health:", 10, 450);
+				g.drawString("" + "Score:", 515, 450);
+				Script.setDrawingLayerToActorLayer(getActor(1));
+			}
+		});
+		
+		/* =========================== Keyboard =========================== */
+		addKeyStateListener("hit", function(pressed:Bool, released:Bool, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled && pressed)
+			{
+				Engine.engine.setGameAttribute("Health", ((Engine.engine.getGameAttribute("Health") : Float) - 1));
+			}
+		});
+		
+		/* ======================== When Updating ========================= */
+		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				if(((Engine.engine.getGameAttribute("Health") : Float) == 2))
+				{
+					getActor(33).setAnimation("2/3");
+				}
+				else if(((Engine.engine.getGameAttribute("Health") : Float) == 1))
+				{
+					getActor(33).setAnimation("1/3");
+				}
+				else if(((Engine.engine.getGameAttribute("Health") : Float) == 0))
+				{
+					getActor(33).setAnimation("Empty");
+				}
+			}
+		});
+		
+	}
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
